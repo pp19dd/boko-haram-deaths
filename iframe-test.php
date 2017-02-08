90e591fd1ab4b77d53cfe9f3c013a5a118489ea9
Dino Beslagic <pp19dd@gmail.com>
2017-02-07 20:30:28 -0500

    Simulate SH conditions

--- iframe-test.php
+++ iframe-test.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>iframe test</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
 <style>
 iframe, h1 { width: 80%; margin-left: 10% }
 </style>
